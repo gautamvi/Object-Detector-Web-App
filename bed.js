ebb03e87146c35760e6e9cabf4cbedc25cbf1f39
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-04-05 18:51:23 -0700

    Add files via upload

--- bed.js
+++ bed.js
@@ -7,4 +7,18 @@ function preload(){
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
 }
